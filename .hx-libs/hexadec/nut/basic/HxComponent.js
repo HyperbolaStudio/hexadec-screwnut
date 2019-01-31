4c8edbc40e2de881a6563f4d5e99f75c25b22512
CrSjimo <crs_16423@outlook.com>
2019-01-31 08:41:04 +0800

    t

--- .hx-libs/hexadec/nut/basic/HxComponent.js
+++ .hx-libs/hexadec/nut/basic/HxComponent.js
@@ -1,32 +1,17 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var HxComponent = /** @class */ (function (_super) {
-    __extends(HxComponent, _super);
-    function HxComponent() {
-        return _super.call(this) || this;
+class HxComponent extends HTMLElement {
+    constructor() {
+        super();
     }
-    HxComponent.prototype.createStyle = function (styleText) {
-        var style = document.createElement('style');
+    createStyle(styleText) {
+        let style = document.createElement('style');
         style.textContent = styleText;
         if (this.shadowRoot) {
             this.shadowRoot.appendChild(style);
         }
         else {
-            throw new Error("Shadow Root is not openen or not exist. try \"attachShadow({mode:'open'})\" first. ");
+            throw new Error(`Shadow Root is not openen or not exist. try "attachShadow({mode:'open'})" first. `);
         }
         return style;
-    };
-    return HxComponent;
-}(HTMLElement));
+    }
+}
--- tstest.js
+++ tstest.js
@@ -1,14 +1,13 @@
 "use strict";
-var txt = "Hello World!";
-var Test = /** @class */ (function () {
-    function Test(x) {
+let txt = "Hello World!";
+class Test {
+    constructor(x) {
         this.x = x;
     }
-    Test.prototype.f = function () {
+    f() {
         console.log(this.x);
         return this.x.length;
-    };
-    return Test;
-}());
-var t = new Test(txt);
+    }
+}
+let t = new Test(txt);
 console.log(t.f());
